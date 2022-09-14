4c98f96b26e2315234b5560bfdbf79b84c80ecbc
Chloe Dawn <chloe@sapphic.dev>
2022-09-14 22:44:36 +0100

    Remove "v1" from CardcastService.BASE_URL

--- src/main/java/net/socialgamer/cah/cardcast/CardcastService.java
+++ src/main/java/net/socialgamer/cah/cardcast/CardcastService.java
@@ -64,7 +64,7 @@ public class CardcastService {
   /**
    * Base URL to the Cardcast API.
    */
-  private static final String BASE_URL = "https://" + HOSTNAME + "/v1/decks/";
+  private static final String BASE_URL = "https://" + HOSTNAME + "/decks/";
   /**
    * URL to the Cardcast API for information about a card set. The only format replacement is the
    * string deck ID.
